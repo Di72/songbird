640ac2c6dad65b7c000ff70c4c1a9cc86896fcdc
Di72 <pro100st111m@gmail.com>
2020-08-30 23:23:34 +0400

    feat: add types

---
+++ songbird/src/types/types.ts
@@ -0,0 +1,13 @@
+export type MainanimType = {
+	name: string;
+	image: string;
+	audio: string;
+};
+export type animsDataType = {
+	id: any;
+	name: string | null;
+	species: string | null;
+	description: string | null;
+	image: string | undefined;
+	audio: string | undefined;
+};
